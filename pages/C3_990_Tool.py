caa7ce9da8f836d20c30c0f132311437d473cea3
Riana <166577991+rleec3@users.noreply.github.com>
2024-04-25 20:05:16 +0000

    naming again

--- pages/C3_990_Tool.py
+++ pages/C3_990_Tool.py
@@ -156,7 +156,7 @@ def edit_excel_template(data, template_path):
 # Streamlit UI components
 banner_path = 'Horizontal_Banner_NoSC.png'
 st.image(banner_path, width=400)
-st.header("c3 990 Tool")
+st.header("C3 990 Tool")
 num_orgs = st.number_input("How many organizations do you want to fetch?", min_value=1, max_value=30, value=1, key="num_orgs")
 # Initialize or reset session state variables as needed
 if 'organizations_data' not in st.session_state:
